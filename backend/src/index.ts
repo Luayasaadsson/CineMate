28ef7dca6f3ec7b92b2ce3bf9e6cb662e403edc2
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-11-27 21:39:33 +0100

    Cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,38 @@
-import express from 'express'
-import cors from 'cors'
-import connectDB from './db/db'
-import authRoutes from './routes/authRoutes'
-import favoriteMoviesRoutes from './routes/favoriteMoviesRoutes'
+import express from "express";
+import cors from "cors";
+import connectDB from "./db/db";
+import authRoutes from "./routes/authRoutes";
+import favoriteMoviesRoutes from "./routes/favoriteMoviesRoutes";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-connectDB()
+const allowedOrigins = ["https://cinemate-dun.vercel.app"];
 
-const PORT = process.env.PORT || 6008
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
 
-app.get('/', (req, res) => {
-  res.send('CineMate API is running...')
-})
+app.use(express.json());
 
-app.use('/auth', authRoutes)
-app.use('/favorites', favoriteMoviesRoutes)
+connectDB();
+
+const PORT = process.env.PORT || 6008;
+
+app.get("/", (req, res) => {
+  res.send("CineMate API is running...");
+});
+
+app.use("/auth", authRoutes);
+app.use("/favorites", favoriteMoviesRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
