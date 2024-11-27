badd149e21f22caf3716455b233c43434c995e62
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-11-27 21:59:34 +0100

    update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,17 +6,11 @@ import favoriteMoviesRoutes from "./routes/favoriteMoviesRoutes";
 
 const app = express();
 
-const allowedOrigins = ["https://cinemate-dun.vercel.app"];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
