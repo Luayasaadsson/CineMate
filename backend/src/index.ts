b2c5c1cb5e83fc3856ab5e65537477a2bf5355e0
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-11-27 22:13:31 +0100

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,8 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 
