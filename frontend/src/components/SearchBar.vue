0de02845e034916c49387cdc1eea3eb66f339653
Luay Asaadsson <155851838+Luayasaadsson@users.noreply.github.com>
2024-12-01 22:37:13 +0100

    Add search icon

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue'
+import { MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
 
 const emit = defineEmits<{
   (e: 'update:searchQuery', value: string): void
@@ -23,13 +24,19 @@ const handleSearch = () => {
 
 <template>
   <div class="search-bar">
-    <input
-      type="text"
-      v-model="localQuery"
-      placeholder="Let's find some movies!"
-      @keydown.enter="handleSearch"
-    />
-    <button @click="handleSearch">Search</button>
+    <div class="input-container">
+      <input
+        type="text"
+        v-model="localQuery"
+        placeholder="Let's find some movies!"
+        @keydown.enter="handleSearch"
+      />
+      <MagnifyingGlassIcon
+        class="search-icon"
+        @click="handleSearch"
+        aria-label="Search"
+      />
+    </div>
   </div>
 </template>
 
@@ -40,25 +47,36 @@ const handleSearch = () => {
   margin: 1rem 0;
 }
 
-.search-bar input {
+.input-container {
+  position: relative;
   width: 800px;
-  padding: 0.5rem;
+}
+
+.input-container input {
+  width: 100%;
+  padding: 0.5rem 2.5rem 0.5rem 0.5rem;
   border: 1px solid #ccc;
-  border-right: none;
-  border-radius: 6px 0 0 4px;
+  border-radius: 6px;
+  border-bottom-right-radius: 17px;
+  border-top-right-radius: 17px;
 }
 
-.search-bar button {
-  padding: 0.5rem 1.5rem;
+.input-container input:focus {
+  outline: none;
+  border-color: var(--btn-bg-dark);
+  box-shadow: 0 0 3px rgba(58, 97, 134, 0.5);
+}
+
+.search-icon {
+  position: absolute;
+  top: 49%;
+  right: 1px;
+  transform: translateY(-50%);
+  width: 1.9rem;
+  height: 1.9rem;
+  color: #fff;
   background: linear-gradient(45deg, #3a6186, #89253e);
-  color: white;
-  border: none;
-  border-left: none;
-  border-radius: 0 6px 4px 0;
+  border-radius: 50%;
   cursor: pointer;
-}
-
-.search-bar button:hover {
-  background-color: #1a242f;
 }
 </style>
